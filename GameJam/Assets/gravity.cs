642916615f76da7e13449c3f71d55f8e74201007
RainerKuusvek <kuusvek.rainer@gmail.com>
2018-01-27 16:31:43 +0000

    Yes

---
+++ GameJam/Assets/gravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity : MonoBehaviour {
+    public float acceleration;
+    SphereCollider thisspherecollider;
+    private void Start()
+    {
+        thisspherecollider = GetComponent<SphereCollider>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Vector2 delta = transform.position - other.transform.position;
+        float distance = delta.magnitude;
+        float localRadius = thisspherecollider.radius * transform.lossyScale.x;
+        float acl = Mathf.Clamp((1 - distance / localRadius) *acceleration, 0, acceleration);
+        other.attachedRigidbody.AddForce(delta.normalized*acl);
+    }
+
+}
---
+++ GameJam/Assets/rocketcontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocketcontroller : MonoBehaviour {
+    public Transform southeast, southwest, northeast, northwest;
+    Rigidbody rigidbod;
+    public float magnitude;
+    void Start () {
+        rigidbod = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbod.AddForceAtPosition(-southeast.forward*magnitude, southeast.position);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbod.AddForceAtPosition(-southwest.forward * magnitude, southwest.position);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbod.AddForceAtPosition(-northwest.forward * magnitude, northwest.position);
+
+        }
+
+    }
+}
